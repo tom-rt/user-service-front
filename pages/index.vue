246381d7188d492fa2203f7edc53d95ff7d08f45
Thomas Raout <thomas.raout@neoxia.com>
2020-06-17 14:58:12 +0200

    router initiated

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Hello world!</h1>
+  <h1> default {{name}}</h1>
 </template>
+
+<script>
+
+export default {
+    name: "test",
+    middleware ({ redirect }) {
+        return redirect('/login')
+        // If the user is not authenticated
+        // if (!store.state.authenticated) {
+        //   return redirect('/login')
+        // }
+  }
+}
+
+</script>
+
+<style>
+</style>
