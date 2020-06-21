b5baed0d4004a7222d5114519784b42e260b8678
Thomas Raout <thomas.raout-ext@pernod-ricard.com>
2020-06-21 23:08:36 +0200

    experiencing

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,8 +1,54 @@
 <template>
-  <h1> Login </h1>
+  <div class="container">
+  
+    <h1>Login</h1>
+
+    <form @submit.prevent="login">
+      <p v-if="formError" class="error">
+        {{ formError }}
+      </p>
+      <p>Username: <input v-model="username" type="text" name="username"></p>
+      <p>Password: <input v-model="password" type="password" name="password"></p>
+      <button type="submit">
+        Login
+      </button>
+    </form>
+
+  </div>  
 </template>
 
 <script>
+import axios from 'axios'
+
+export default {
+data () {
+    return {
+      formError: null,
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        console.log(this.username)
+        console.log(this.password)
+
+        // axios.defaults.headers.post['Content-Type'] ="application/json";
+        // axios.defaults.headers.post['Access-Control-Allow-Origin'] = "*";
+
+        const data = { name: this.username, password: this.password };
+        
+        const ret = await axios.post('http://localhost:8081/v1/user', data)
+
+        console.log("ret", ret)
+
+      } catch (e) {
+        this.formError = e.message
+      }
+    },
+  }
+}
 </script>
 
 <style>
