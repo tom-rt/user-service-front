f6ba17709bf7709e5cf4c71f5fb602474413941b
Thomas Raout <thomas.raout@neoxia.com>
2020-06-22 17:38:47 +0200

    first store handling

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
     env: {
         baseUrl: process.env.BASE_URL || 'http://localhost:8081'
-    }
+    },
+    build: {
+        additionalExtensions: ['ts', 'tsx'],
+    },
 }
--- pages/home/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1> HOME </h1>
-</template>
-
-<script>
-</script>
-
-<style>
-</style>
