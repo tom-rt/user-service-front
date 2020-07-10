fceecdf2bf2e55f14aebed03c0d58e7ffed933fb
Thomas Raout <thomas.raout-ext@pernod-ricard.com>
2020-07-10 18:01:40 +0200

    original request resent

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -7,20 +7,21 @@ export default ({ $axios, store, redirect }) => {
    })
 
    $axios.onResponseError(async err => {
+      console.log(err);
       if (err.response.status == 401 && err.response.data.message == "Token expired.") {
          const refresh = await $axios.post('http://localhost:8081/v1/refresh/token')
          if (refresh.status == 200) {
             $axios.setToken(refresh.data.token, 'Bearer')
+            let originalRequest = err.config;
+            originalRequest.headers['Authorization'] = 'Bearer ' + refresh.data.token;
+            return $axios(originalRequest);
          } else {
             store.state.token = null;
             store.state.id = null;
             redirect("/login")
          }
-         // set token
-         return err
       } else {
          return err
       }
-
    })
 }
